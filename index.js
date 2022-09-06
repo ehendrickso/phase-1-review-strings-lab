621dc6e2b4786c2571526fa5926374fb7497d800
+ <erol.hendrickson@gmail.com>
2022-09-06 17:14:37 -0500

    commit tree 8ac0a7511d45e8d669822307bf9b543d8ef54de5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+const currentUser = "User"
+const excitedUser = currentUser.toUpperCase()
+
+const firstLetterOfUser = currentUser.charAt(0)
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + excitedUser + "!"
+
+const shortGreeting = "Welcome, "+ firstLetterOfUser + "!"
